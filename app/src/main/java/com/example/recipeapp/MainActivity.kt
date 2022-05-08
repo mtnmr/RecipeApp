c7a07e2b9d253b77a247d9de64b49c4787f06599
mtnmr <mei.rila1013@gmail.com>
2022-05-08 21:38:40 +0900

    recipeEditFragment(title, spinner)

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipeapp.databinding.FragmentRecipeEditBinding
+import com.example.recipeapp.ui.RecipeEditFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = RecipeEditFragment()
+        fragmentTransaction.add(R.id.container, fragment)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/example/recipeapp/ui/RecipeEditFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/RecipeEditFragment.kt
@@ -5,16 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.recipeapp.R
+import com.example.recipeapp.databinding.FragmentRecipeEditBinding
 
 class RecipeEditFragment : Fragment() {
+
+    private var _binding:FragmentRecipeEditBinding ?= null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_edit, container, false)
+        _binding = FragmentRecipeEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.recipe_category_list,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinner = binding.categorySpinner
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object :  AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                val spinner = parent as Spinner
+                val str = spinner.selectedItem.toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
