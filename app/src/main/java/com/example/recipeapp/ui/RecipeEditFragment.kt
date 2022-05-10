1bc63273f755c18f832c32e04adf5851393d6a60
mtnmr <mei.rila1013@gmail.com>
2022-05-10 16:36:19 +0900

    take picture

--- app/src/main/java/com/example/recipeapp/ui/RecipeEditFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/RecipeEditFragment.kt
@@ -4,26 +4,28 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.app.DatePickerDialog
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.hardware.input.InputManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import androidx.navigation.fragment.findNavController
 import com.example.recipeapp.R
 import com.example.recipeapp.databinding.FragmentRecipeEditBinding
+import java.io.File
 import java.lang.Exception
+import java.text.SimpleDateFormat
 import java.util.*
 
 class RecipeEditFragment : Fragment() {
@@ -66,7 +68,8 @@ class RecipeEditFragment : Fragment() {
 
 
         binding.recipeImage.setOnClickListener {
-            checkPermission()
+//            checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            checkPermission(Manifest.permission.CAMERA)
         }
 
 
@@ -86,16 +89,19 @@ class RecipeEditFragment : Fragment() {
     }
 
     //パーミッション許可
-    private fun checkPermission(){
+    private fun checkPermission(permission: String){
         if (ActivityCompat.checkSelfPermission(requireContext(),
-            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-            selectPhoto()
+            permission) == PackageManager.PERMISSION_GRANTED){
+            when(permission) {
+                Manifest.permission.READ_EXTERNAL_STORAGE -> selectPhoto()
+                Manifest.permission.CAMERA -> takePicture()
+            }
         }else{
-           requestPermission()
+           requestPermission(permission)
         }
     }
 
-    private val requestPermissionLauncher =
+    private val storagePermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()){
                 isGranted:Boolean ->
             if (isGranted){
@@ -108,12 +114,40 @@ class RecipeEditFragment : Fragment() {
             }
         }
 
-    private fun requestPermission(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)){
-            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val cameraPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted : Boolean ->
+        if (isGranted){
+            Toast.makeText(activity?.applicationContext, "デバイス内の写真やメディアへのアクセスが許可されました。", Toast.LENGTH_SHORT).show()
+            Log.d("recipeApp", "camera permission OK")
+            takePicture()
         }else{
-            //説明を表示する処理？
-            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            Toast.makeText(activity?.applicationContext , "デバイス内の写真やメディアへのアクセスが許可されませんでした。", Toast.LENGTH_SHORT).show()
+            Log.d("recipeApp", "camera permission NG")
+        }
+        }
+
+    private fun requestPermission(permission: String){
+        when(permission){
+            Manifest.permission.READ_EXTERNAL_STORAGE -> {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), permission)
+                ) {
+                    storagePermissionLauncher.launch(permission)
+                } else {
+                    //説明を表示する処理？
+                    storagePermissionLauncher.launch(permission)
+                }
+            }
+
+            Manifest.permission.CAMERA -> {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), permission)
+                ) {
+                    cameraPermissionLauncher.launch(permission)
+                } else {
+                    //説明を表示する処理？
+                    cameraPermissionLauncher.launch(permission)
+                }
+            }
         }
     }
 
@@ -125,7 +159,7 @@ class RecipeEditFragment : Fragment() {
                 try{
                     binding.recipeImage.setImageURI(result.data?.data)
                 }catch (e:Exception){
-                    Toast.makeText(activity?.applicationContext, "Error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity?.applicationContext, "select photo Error", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -136,6 +170,59 @@ class RecipeEditFragment : Fragment() {
             type = "image/*"
         }
         selectPhotoLauncher.launch(intent)
+    }
+
+    private var cameraUri: Uri? = null
+
+    private val takePictureLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode != RESULT_OK){
+                return@registerForActivityResult
+            }else{
+                try{
+                    val data = result.data
+                    if (data != null){
+                        if(cameraUri != null ){
+                            binding.recipeImage.setImageURI(cameraUri)
+                            Log.d("recipe app", "${cameraUri}")
+                        }else{
+                            Log.d("recipe app", "cameraUri null")
+                        }
+                    }
+//                    val data = result.data?.extras?.get("data") as Bitmap
+//                    if (data != null){
+//                        binding.recipeImage.setImageBitmap(data)
+//                        Log.d("recipe app", "picture set")
+//                    }
+                }catch (e:Exception){
+                    Toast.makeText(activity?.applicationContext, "set photo Error", Toast.LENGTH_SHORT).show()
+                    Log.d("recipe app", e.toString())
+                }
+            }
+        }
+
+    private fun takePicture(){
+//        val dir:File? = activity?.applicationContext?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        //内部ストレージPicturesに保存される
+//        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        //内部ストレージDCIMに保存される　→　画像から見れる
+        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/Camera")
+        val fileDate:String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val file:File = File.createTempFile("JPEG_${fileDate}_", ".jpg", dir)
+        cameraUri = FileProvider.getUriForFile(
+            activity?.applicationContext!!,
+            "com.example.recipeapp.fileprovider",
+            file
+        )
+
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+            putExtra(MediaStore.EXTRA_OUTPUT, cameraUri)
+        }
+        takePictureLauncher.launch(intent)
+        Log.d("recipe app", "take picture intent")
+
     }
 
 
