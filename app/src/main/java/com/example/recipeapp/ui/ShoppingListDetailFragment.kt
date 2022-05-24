948bee50f3758968acb2363766b62dfbd86eacf8
mtnmr <mei.rila1013@gmail.com>
2022-05-24 11:38:44 +0900

    shopping list ui

---
+++ app/src/main/java/com/example/recipeapp/ui/ShoppingListDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.example.recipeapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.R
+import com.example.recipeapp.databinding.FragmentShoppingListDetailBinding
+
+class ShoppingListDetailFragment : Fragment() {
+
+    private var _binding : FragmentShoppingListDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentShoppingListDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/recipeapp/ui/ShoppingListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.recipeapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.R
+import com.example.recipeapp.databinding.FragmentShoppingListBinding
+
+class ShoppingListFragment : Fragment() {
+
+    private var _binding : FragmentShoppingListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentShoppingListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
